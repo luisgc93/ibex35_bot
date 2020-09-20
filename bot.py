b9111554132c12c8d48c324bfde37fe8450ba644
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-09-20 15:41:48 +0200

    extract parsing method

--- bot.py
+++ bot.py
@@ -18,17 +18,21 @@ oauth = OAuth(
 t = Twitter(auth=oauth)
 
 
+def parse_links(links):
+    link = set(filter(lambda x: 'autor' not in x, links)).pop()
+    if link.startswith('//'):
+        link = 'https://' + link[2:]
+    return link
+
 def scrape_el_economista():
     response = requests.get('https://www.eleconomista.es/mercados-cotizaciones/', headers=HEADERS)
     tree = fromstring(response.content)
     links = tree.xpath('//div[@class="firstContent-centerColumn col-xl-5 col-lg-5 col-md-12 col-12 order-1 order-md-1 order-lg-2"]//a/@href')
-    link = links[0]
-    if link.startswith('//'):
-        link = 'https://' + link[2:]
+    link = parse_links(links)
     response = requests.get(link, headers=HEADERS)
     blog_tree = fromstring(response.content)
     para = blog_tree.xpath('//p/text()')[0]
-    sub_header = blog_tree.xpath('//h2/text()')[3]
+    sub_header = blog_tree.xpath('//h2/text()')[0]
     tokenized_para = tokenizer.tokenize(para)
     text = sub_header + '. ' + random.choice(tokenized_para)
     t.statuses.update(status=f"{text[:TW_CHAR_LIMIT]}{link}")
