33854871c96306fdf7373621b63b1944ed8175bc
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-09-27 16:16:56 +0200

    we'll use this to test our prod env

--- src/clock.py
+++ src/clock.py
@@ -5,6 +5,11 @@ from src import bot
 sched = BlockingScheduler()
 
 
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    bot.scrape_el_economista()
+
+
 @sched.scheduled_job("cron", day_of_week="mon-sat", hour=10)
 def scheduled_job():
     bot.scrape_el_economista()
