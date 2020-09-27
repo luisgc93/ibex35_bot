59234ce1759bffc9661a6c133534b8c05c051d58
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-09-27 18:43:45 +0200

    fix use of  os.walk

--- src/clock.py
+++ src/clock.py
@@ -1,8 +1,13 @@
 import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-os.walk()
-print(os.walk)
+import os
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
+
 from src import bot
 
 sched = BlockingScheduler()
