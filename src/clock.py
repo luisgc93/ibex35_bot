04bf83e7fa105d91cf137825087bb147fb70e2cd
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-09-27 18:38:14 +0200

    let's try to print all directories

--- src/clock.py
+++ src/clock.py
@@ -1,18 +1,21 @@
+import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from src.bot import scrape_el_economista
+os.walk()
+print(os.walk)
+from src import bot
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job("interval", minutes=2)
 def timed_job():
-    scrape_el_economista()
+    bot.scrape_el_economista()
 
 
 @sched.scheduled_job("cron", day_of_week="mon-sat", hour=10)
 def scheduled_job():
-    scrape_el_economista()
+    bot.scrape_el_economista()
 
 
 sched.start()
