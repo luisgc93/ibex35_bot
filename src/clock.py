a6ccc3c1fdd8a9d4e017e8cb106b2329014ce170
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-09-27 18:29:54 +0200

    (hopefully) fix path

--- src/clock.py
+++ src/clock.py
@@ -1,18 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from . import bot
+from src.bot import scrape_el_economista
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job("interval", minutes=2)
 def timed_job():
-    bot.scrape_el_economista()
+    scrape_el_economista()
 
 
 @sched.scheduled_job("cron", day_of_week="mon-sat", hour=10)
 def scheduled_job():
-    bot.scrape_el_economista()
+    scrape_el_economista()
 
 
 sched.start()
