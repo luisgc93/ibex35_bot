4cc801b9df2cf5dd194cfe00e95d036a2b5bd692
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-10-03 14:12:11 +0200

    remove old logic

--- src/bot.py
+++ src/bot.py
@@ -9,42 +9,8 @@ from twitter import OAuth, Twitter
 
 from . import const
 
-nltk.download("punkt")
 
-tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
-
-oauth = OAuth(
-    os.environ.get("ACCESS_TOKEN"),
-    os.environ.get("ACCESS_TOKEN_SECRET"),
-    os.environ.get("CONSUMER_KEY"),
-    os.environ.get("CONSUMER_SECRET"),
-)
-t = Twitter(auth=oauth)
-
-
-def extract_url(urls):
-    url = set(filter(lambda x: "autor" not in x, urls)).pop()
-    if url.startswith("//"):
-        url = "https://" + url[2:]
-    return url
-
-
-def scrape_el_economista():
-    home_url = "https://www.eleconomista.es/mercados-cotizaciones/"
-    xpath = (
-        '//div[@class="firstContent-centerColumn '
-        "col-xl-5 col-lg-5 col-md-12 col-12 order-1 order-md-1 "
-        'order-lg-2"]//a/@href'
-    )
-    article = get_article_url(home_url, xpath)
-    para = extract_paragraph(article)
-    status = generate_status(para, article)
-    publish_tweet(status)
-
-
-def scrape_bolsamania():
-    home_url = "https://www.bolsamania.com/indice/IBEX-35/noticias"
-    xpath = "//article/header/h2/a/@href"
+def scrape_website(home_url, xpath):
     article = get_article_url(home_url, xpath)
     para = extract_paragraph(article)
     status = generate_status(para, article)
