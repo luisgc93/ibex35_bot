c279ba6d57211de6ac452b6c35b61652d531fa0c
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-10-03 14:12:30 +0200

    add SITES constant

--- src/const.py
+++ src/const.py
@@ -5,3 +5,16 @@ HEADERS = {
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) "
     "Safari/537.36"
 }
+
+SITES = [
+    {
+        "home_url": "https://www.eleconomista.es/mercados-cotizaciones/",
+        "xpath":         '//div[@class="firstContent-centerColumn '
+                         "col-xl-5 col-lg-5 col-md-12 col-12 order-1 order-md-1 "
+                         'order-lg-2"]//a/@href',
+     },
+    {
+        "home_url": "https://www.bolsamania.com/indice/IBEX-35/noticias",
+        "xpath": "//article/header/h2/a/@href"
+    }
+]
