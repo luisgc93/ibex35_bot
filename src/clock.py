ab3ea585f05f3dcea4f944ea2dbcf8235192e139
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-07 16:07:01 +0100

    move reply to mentions to its own job

--- src/clock.py
+++ src/clock.py
@@ -7,13 +7,17 @@ from . import bot, const
 sched = BlockingScheduler()
 
 
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    bot.reply_to_mentions()
+
+
 @sched.scheduled_job("cron", day_of_week="mon-sat", hour=10)
 def scheduled_job():
     site = random.choice(const.SITES)
     home_url = site.get("home_url")
     xpath = site.get("xpath")
     bot.scrape_website(home_url, xpath)
-    bot.reply_to_mentions()
 
 
 def main():
