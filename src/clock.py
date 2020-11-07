1f941b8d5c274fe76b26a56e1096fa3af4035543
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-07 13:48:30 +0100

    removing this

--- src/clock.py
+++ src/clock.py
@@ -7,14 +7,6 @@ from . import bot, const
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    site = random.choice(const.SITES)
-    home_url = site.get("home_url")
-    xpath = site.get("xpath")
-    bot.scrape_website(home_url, xpath)
-
-
 @sched.scheduled_job("cron", day_of_week="mon-sat", hour=10)
 def scheduled_job():
     site = random.choice(const.SITES)
