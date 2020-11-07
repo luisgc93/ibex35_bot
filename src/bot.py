183ee600f9de32f588a6c6c7c99e33573cac685b
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-07 18:35:13 +0100

    Add punctuation

--- src/bot.py
+++ src/bot.py
@@ -71,7 +71,7 @@ def reply_to_mentions():
             stock_name = parse_stock_name(tweet)
             stock_price = get_stock_price(stock_name)
             if stock_price:
-                status = f"@{user} Las acciones de ${stock_name} cotizan a {stock_price}"
+                status = f"@{user} Las acciones de ${stock_name} cotizan a {stock_price}."
             else:
                 status = (
                     f"@{user} No he podido encontrar el precio de ${stock_name}. "
