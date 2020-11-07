221b98a2964416a67860e5bdb28017f20212422a
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-07 18:45:37 +0100

    let's increase this for now

--- src/clock.py
+++ src/clock.py
@@ -7,7 +7,7 @@ from . import bot, const
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=3)
 def timed_job():
     bot.reply_to_mentions()
 
