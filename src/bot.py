39efa3011db3fe28c4927fc889cd5a1dc1da896f
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-15 11:45:28 +0100

    adding this to test logs

--- src/bot.py
+++ src/bot.py
@@ -74,6 +74,7 @@ def shorten_text(text):
 
 
 def reply_to_mentions():
+    logger.info("Test logs")
     mentions = api.mentions_timeline(since_id=1)
     for mention in mentions:
         if mention_has_been_replied(mention.id):
