07827b1b65014d499d9627d5807345e7efb4cc6b
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-27 15:52:07 +0100

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
-from unittest.mock import patch
+import datetime
+from unittest.mock import patch, Mock, MagicMock
 
 import pytest
 from peewee import SqliteDatabase
+from tweepy import Status, User
+
 from src.models import Mention
 
 MODELS = [Mention]
@@ -23,12 +26,43 @@ def mock_env_variables(monkeypatch):
     monkeypatch.setenv("ACCESS_TOKEN_SECRET", "123")
 
 
+@pytest.fixture
+def status(twitter_user):
+    tweet = Status()
+    tweet.id = 1
+    tweet.text = 'What is the current price of $BABA?'
+    tweet.user = twitter_user
+    return tweet
+
+
 @pytest.fixture(autouse=True)
-def mock_tweepy():
+def mock_tweepy(status):
     with patch('src.bot.init_tweepy') as mock:
         yield mock
 
 
 @pytest.fixture
-def mention_1():
-    return Mention(tweet_id=1).save()
+def mock_not_replied_mention(mock_tweepy, status):
+    mock_tweepy.return_value.mentions_timeline.return_value = [status]
+    return mock_tweepy
+
+
+@pytest.fixture
+def mock_replied_mention(mock_tweepy, status):
+    Mention.create(tweet_id=status.id)
+    mock_tweepy.return_value.mentions_timeline.return_value = []
+    return mock_tweepy
+
+
+@pytest.fixture
+def twitter_user():
+    user = User()
+    user.screen_name = 'user_name'
+    return user
+
+
+@pytest.fixture
+def mock_get_price():
+    with patch('src.bot.get_stock_price') as mock:
+        mock.return_value = '$277.72'
+        yield mock
