9f812da6ad389febf9e211f123e18be492c531fd
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-27 15:52:16 +0100

    implement tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,4 +1,9 @@
+from unittest.mock import call
+
 import pytest
+
+from src.models import Mention
+from src import bot
 
 
 class TestArticlesFeature:
@@ -14,14 +19,50 @@ class TestArticlesFeature:
 
 
 class TestStocksFeature:
-    def test_returns_stock_name_when_tweet_contains_stock(self):
-        raise NotImplementedError
+    @pytest.mark.parametrize("tweet, stock_name", [
+        ("What is the price of $AMZN?", "AMZN"),
+        ("How much is $WMT right now?", "WMT")
+    ])
+    def test_returns_stock_name_when_tweet_contains_stock(self, tweet, stock_name):
+        assert bot.parse_stock_name(tweet) == stock_name
 
-    def test_updates_status_when_mention_has_not_been_replied(self):
-        raise NotImplementedError
+    @pytest.mark.usefixtures("mock_get_price", "mock_not_replied_mention")
+    def test_updates_status_when_mention_has_not_been_replied(self, mock_tweepy):
+        bot.reply_to_mentions()
 
-    def test_does_not_update_status_when_mention_has_been_replied(self):
-        raise NotImplementedError
+        mock_tweepy.assert_has_calls(
+            [
+                call().update_status(
+                    status="@user_name Las acciones de $BABA cotizan a $277.72.",
+                    in_reply_to_status_id=1,
+                )
+            ]
+        )
 
-    def test_saves_new_mentions(self):
-        raise NotImplementedError
+    @pytest.mark.usefixtures("mock_get_price", "mock_not_replied_mention")
+    def test_saves_mention_when_mention_has_not_been_replied(self, mock_tweepy):
+        assert Mention.select().count() == 0
+
+        bot.reply_to_mentions()
+
+        mock_tweepy.assert_has_calls([call().mentions_timeline(since_id=None)])
+        assert Mention.select().count() == 1
+
+    @pytest.mark.usefixtures("mock_replied_mention")
+    def test_does_not_update_status_when_mention_has_been_replied(self, mock_tweepy):
+        update_status_call = call().update_status(
+            status="@user_name Las acciones de $BABA cotizan a $277.72.",
+            in_reply_to_status_id=1,
+        )
+
+        bot.reply_to_mentions()
+
+        assert update_status_call not in mock_tweepy.mock_calls
+
+    @pytest.mark.usefixtures("mock_replied_mention")
+    def test_does_not_save_mention_when_mention_has_been_replied(self):
+        assert Mention.select().count() == 1
+
+        bot.reply_to_mentions()
+
+        assert Mention.select().count() == 1
