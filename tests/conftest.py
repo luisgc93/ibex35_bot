4b8df18c5e48f1a7230d6b2d30855f48ada79806
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-11-29 20:33:47 +0100

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 from peewee import SqliteDatabase
 from tweepy import Status, User
 
+from src.const import API_LIMIT_EXCEEDED_MESSAGE, STOCK_NOT_FOUND_MESSAGE
 from src.models import Mention
 
 MODELS = [Mention]
@@ -23,6 +24,7 @@ def mock_env_variables(monkeypatch):
     monkeypatch.setenv("CONSUMER_SECRET", "123")
     monkeypatch.setenv("ACCESS_TOKEN", "123")
     monkeypatch.setenv("ACCESS_TOKEN_SECRET", "123")
+    monkeypatch.setenv("ALPHA_VANTAGE_API_KEY", "123")
 
 
 @pytest.fixture(autouse=True)
@@ -54,10 +56,64 @@ def mock_new_mention(mock_tweepy, status):
 
 
 @pytest.fixture
+def mock_mention_with_invalid_stock_name(mock_tweepy, status):
+    status.text = "What is the price of $FOO?"
+    mock_tweepy.return_value.mentions_timeline.return_value = [status]
+    return mock_tweepy
+
+
+@pytest.fixture
 def mock_replied_mention(mock_tweepy, status):
     Mention.create(tweet_id=status.id)
     mock_tweepy.return_value.mentions_timeline.return_value = []
     return mock_tweepy
+
+
+@pytest.fixture
+def mock_alpha_vantage_get_intra_day():
+    with patch("alpha_vantage.timeseries.TimeSeries.get_intraday") as mock:
+        mock.return_value = (
+            {
+                "2020-11-27 16:55:00": {
+                    "1. open": "276.8000",
+                    "2. high": "276.8000",
+                    "3. low": "276.8000",
+                    "4. close": "276.8000",
+                    "5. volume": "513",
+                },
+                "2020-11-27 16:50:00": {
+                    "1. open": "276.9000",
+                    "2. high": "276.9800",
+                    "3. low": "276.8400",
+                    "4. close": "276.9800",
+                    "5. volume": "754",
+                },
+            },
+            {
+                "1. Information": "Intraday (15min) open, high, low, "
+                "close prices and volume",
+                "2. Symbol": "BABA",
+                "3. Last Refreshed": "2020-11-27 17:00:00",
+                "4. Interval": "15min",
+                "5. Output Size": "Compact",
+                "6. Time Zone": "US/Eastern",
+            },
+        )
+        yield mock
+
+
+@pytest.fixture
+def mock_alpha_vantage_max_retries_exceeded():
+    with patch("alpha_vantage.timeseries.TimeSeries.get_intraday") as mock:
+        mock.side_effect = ValueError(API_LIMIT_EXCEEDED_MESSAGE)
+        yield mock
+
+
+@pytest.fixture
+def mock_alpha_vantage_stock_not_found():
+    with patch("alpha_vantage.timeseries.TimeSeries.get_intraday") as mock:
+        mock.side_effect = ValueError(STOCK_NOT_FOUND_MESSAGE)
+        yield mock
 
 
 @pytest.fixture
